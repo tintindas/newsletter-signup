3bd627d974fe88d698c0b23a48104239ce82e416
Upamanyu Das <upamanyu.das.work@gmail.com>
2019-12-29 19:47:05 +0530

    Initial commit.

---
+++ index.js
@@ -0,0 +1,63 @@
+//jshint esversion:6
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+
+  var data = {
+    members: [{
+      email_address: email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: lastName
+      }
+    }]
+  };
+
+  var jsonData = JSON.stringify(data);
+
+  var options = {
+    url: "https://us4.api.mailchimp.com/3.0/lists/d35f3dd601",
+    method: 'post',
+    headers: {
+      "Authorization": "tintin_das afec758c7682adae62a18fd567fa7723-us4"
+    },
+    body: jsonData
+  };
+
+  request(options, function(error, response, body) {
+    if (error) {
+      res.sendFile(__dirname + "/failure.html");
+    } else if(response.statusCode === 200){
+      res.sendFile(__dirname + "/success.html");
+    } else{
+      res.sendFile(__dirname + "/failure.html");
+    }
+  });
+});
+
+app.post("/failure", function(req, res){
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server running on port 3000");
+});
